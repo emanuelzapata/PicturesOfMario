0d71ea9161d0693d159c931b5e19c185a1d2c942
Emanuel Zapata <emanuelzapata@ymail.com>
2021-03-20 14:40:48 -0500

    updated to get by id

--- app.py
+++ app.py
@@ -1,24 +1,46 @@
-from flask import Flask, render_template, send_file
+from flask import Flask, render_template, send_file, request, jsonify
 import os
 from os import listdir
 from os.path import isfile, join
 import random
+import json 
 
 app = Flask(__name__)
 
 
 @app.route('/')
 def index():
-    return render_template('index.html')
+    return render_template('index.html', passed="variablename")
+
+@app.route('/generate_json')
+def generate_json():
+    data = {}
+    data['marios'] = []
+    for i in range(44):
+        data['marios'].append(  {
+            "imageID": i+1,
+            "rarity": 10,
+            "era": "post weight loss"
+        })
+    with open('marios.json', 'w') as outfile:
+        json.dump(data, outfile)
+    return "done"
+
+@app.route('/sample')
+def sample():
+    return send_file('./imgs/21.jpg', mimetype='image/jpg')
+
+@app.route('/get_mario',methods=['GET'])
+def return_mario():
+    marios = json.load(open("marios.json"))
+    mario = random.choice(marios)
+    return jsonify(mario)
 
 @app.route('/api/v1/return_image',methods=['GET'])
-def get_image():
-    #onlyfiles = [f for f in listdir(os.getcwd()+"\\imgs\\") if isfile(join(os.getcwd()+"\\imgs\\",f))]
-    onlyfiles = [f for f in listdir('./imgs') if isfile(join('./imgs',f))]
-    #image_source = "{0}\\imgs\\{1}".format(os.getcwd(),random.choice(onlyfiles))
-    image_source = "./imgs/{0}".format(random.choice(onlyfiles))
+def return_image():
+    image_id = request.args.get('id')
+    #onlyfiles = [f for f in listdir('./imgs') if isfile(join('./imgs',f))]
+    image_source = "./imgs/{0}.jpg".format(image_id)
     return send_file(image_source, mimetype='image/jpg')
-
-
 
 if __name__ == '__main__': app.run(debug=True)
