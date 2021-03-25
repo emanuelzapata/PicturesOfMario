475e5be1f1202b24a9ab73fb9cde7b93d4bd9ba6
Emanuel Zapata <emanuelzapata@ymail.com>
2021-03-25 01:17:17 -0500

    committing updates to get image data and video  playing with mediatype

--- app.py
+++ app.py
@@ -7,8 +7,24 @@ import json
 
 app = Flask(__name__)
 
+def generateJSON():
+    jasonVals = []
+    for i in range(63):
+        print(i+1)
+        jasonVals.append({
+                "imageID": i+1,
+                "rarity": 10,
+                "era": "post weight loss",
+                "mediatype":"jpg"
+        })
+    with open('marios.json', 'w') as outfile:
+        json.dump(jasonVals, outfile)
+marios = json.load(open("marios.json"))
+
 @app.route('/')
 def index():
+    #generateJSON()
+    #print(marios['45'])
     return render_template('index.html', passed="variablename")
 
 @app.route('/api/v1/sample')
@@ -26,8 +42,10 @@ def return_mario():
 @app.route('/api/v1/return_image',methods=['GET'])
 def return_image():
     image_id = request.args.get('id')
+
     #onlyfiles = [f for f in listdir('./imgs') if isfile(join('./imgs',f))]
-    image_source = "./imgs/{0}.jpg".format(image_id)
+    print(marios[image_id])
+    image_source = "./imgs/{0}.{1}".format(image_id,marios[image_id]['mediatype'])
     return send_file(image_source)
 
 if __name__ == '__main__': app.run(debug=True)
